9844417764fe8410dc05e968445d9063b7d3ef89
Richard Sunden <djrap1dtecno@gmail.com>
2019-10-16 17:32:33 -0400

    7.2 Complete

---
+++ WeatherView/ListVC.swift
@@ -0,0 +1,19 @@
+//
+//  ListVC.swift
+//  WeatherView
+//
+//  Created by Richard Sunden on 10/16/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ WeatherView/PageVC.swift
@@ -0,0 +1,19 @@
+//
+//  PageVC.swift
+//  WeatherView
+//
+//  Created by Richard Sunden on 10/16/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
