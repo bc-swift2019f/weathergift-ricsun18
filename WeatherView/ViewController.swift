bda0719755117281d4e173e1687cbfc3f2266d1e
Richard Sunden <djrap1dtecno@gmail.com>
2019-10-16 16:46:47 -0400

    Initial Commit

---
+++ WeatherView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherView
+//
+//  Created by Richard Sunden on 10/16/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
