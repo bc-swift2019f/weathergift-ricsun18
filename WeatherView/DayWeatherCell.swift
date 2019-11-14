816e66914175a5b23f666b117482108786bf2fb0
Richard Sunden <djrap1dtecno@gmail.com>
2019-11-13 23:07:36 -0500

    7.20 Complete

---
+++ WeatherView/DayWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayWeatherCell.swift
+//  WeatherView
+//
+//  Created by Richard Sunden on 11/13/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekDay: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
