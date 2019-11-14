8ec941cae8e16cdbac896f6a6515249e15f88597
Richard Sunden <djrap1dtecno@gmail.com>
2019-11-13 20:09:34 -0500

    7.13 Complete*

---
+++ WeatherView/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherView
+//
+//  Created by Richard Sunden on 11/13/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
